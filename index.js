075b6b98f3d0d011af1619fd9c8de1c776b1f7ef
m <you@example.com>
2018-10-18 06:18:52 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const container = document.getElementById('app')
+const body = <h1> Hi men </h1>;
+// ReactDOM.render(que voy a renderizar, donde lo haré)
+ReactDOM.render(body,container);
+//console.log('Hi world')
+
